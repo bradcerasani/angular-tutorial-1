e824e35c370331bda8690d0f11e6755f125c15d8
Brad Cerasani <bradcerasani@gmail.com>
2014-07-04 00:12:16 -0500

    added app.js controller

---
+++ app.js
@@ -0,0 +1,17 @@
+function BookController($scope) {
+  $scope.books = [
+    {
+      'name': 'Effective Java',
+      'author': 'Joshua Bloch'
+    }, {
+      'name': 'Year without Pants',
+      'author': 'Scott Berkun'
+    }, {
+      'name': 'Confessions of a Public Speaker',
+      'author': 'Scott Berkun'
+    }, {
+      'name': 'Javascript, the Good parts',
+      'author': 'Douglas Crockford'
+    }
+  ]
+}
